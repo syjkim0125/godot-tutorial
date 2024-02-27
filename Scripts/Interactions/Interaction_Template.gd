51ff4ea6613761e6948eb063c31098e87765fbfb
Joshua <syjkim0125@gmail.com>
2024-02-27 17:27:41 +0900

    change script's name

--- Scripts/Interactions/Interaction_Template.gd
+++ Scripts/Interactions/Interaction_Template.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var destination = $Marker2D.get_global_position()
 @onready var animationPlayer: AnimationPlayer = $Sprite2D/AnimationPlayer
 @export var interaction_animation: String
+@export var right: bool
 var interaction_type = "box"
 
 func interact():
