a614501cdb7fd8a5463a031515333804129efb69
Joshua <syjkim0125@gmail.com>
2024-03-20 18:30:23 +0900

    add inventory backpack button toggle function that change backpack image

---
+++ Scripts/Inventory/BackPackButton.gd
@@ -0,0 +1,11 @@
+extends Button
+
+var openTexture = preload("res://Art/UI/Inventory/BackPackOpen.png")
+var closedTexture = preload("res://Art/UI/Inventory/BackPackClosed.png")
+
+func _on_toggled(button_pressed):
+	print('button_pressed', button_pressed)
+	if(button_pressed):
+		set_button_icon(openTexture)
+	else:
+		set_button_icon(closedTexture)
